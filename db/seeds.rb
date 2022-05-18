0fd266070a6d2b61b4409ef62afd19533eb5e2e2
Cédric le Brun <lebruncedric8@gmail.com>
2022-05-18 15:24:15 +0200

    Restaurants reviews done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(name: 'Papill', category: 'french', address: 'Nantes')
+
+Restaurant.create(name: 'Pizzaiolo', category: 'italy', address: 'Rome')
+
+Restaurant.create(name: 'La Muraille', category: 'chinese', address: 'Pekin')
+
+Restaurant.create(name: 'La Friterie', category: 'belgian', address: 'Bruxelles')
+
+Restaurant.create(name: 'Sushis Express', category: 'french', address: 'Tokyo')
